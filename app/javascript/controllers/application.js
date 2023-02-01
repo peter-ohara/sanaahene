03342453c97d426d4f6d1edc9551a08e7f09ec04
Peter O'Hara Adu <ohara.invent@gmail.com>
2023-02-01 09:50:31 +0000

    Add slim and tailwindcsscomponents and regenerate view files

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -6,4 +6,8 @@ const application = Application.start()
 application.debug = false
 window.Stimulus   = application
 
+// Import and register TailwindCSS Components
+import { Slideover } from "tailwindcss-stimulus-components"
+application.register('slideover', Slideover)
+
 export { application }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  fontFamily: {
+    sans: ["Roboto", "sans-serif"],
+    serif: ["Roboto Slab", "serif"],
+    body: ["Roboto", "sans-serif"],
+  },
   content: [
     './app/views/**/*.html.erb',
+    './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
