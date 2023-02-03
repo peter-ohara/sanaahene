ad15c56639dd0280abea9b6fe525789e6f8da5dc
Peter O'Hara Adu <ohara.invent@gmail.com>
2023-02-03 13:42:26 +0000

    Remove jbuilder

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,12 @@
 class TagsController < ApplicationController
   before_action :set_tag, only: %i[ show edit update destroy ]
 
-  # GET /tags or /tags.json
+  # GET /tags
   def index
     @tags = Tag.all
   end
 
-  # GET /tags/1 or /tags/1.json
+  # GET /tags/1
   def show
   end
 
@@ -19,41 +19,36 @@ class TagsController < ApplicationController
   def edit
   end
 
-  # POST /tags or /tags.json
+  # POST /tags
   def create
     @tag = Tag.new(tag_params)
 
     respond_to do |format|
       if @tag.save
         format.html { redirect_to tag_url(@tag), notice: "Tag was successfully created." }
-        format.json { render :show, status: :created, location: @tag }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /tags/1 or /tags/1.json
+  # PATCH/PUT /tags/1
   def update
     respond_to do |format|
       if @tag.update(tag_params)
         format.html { redirect_to tag_url(@tag), notice: "Tag was successfully updated." }
-        format.json { render :show, status: :ok, location: @tag }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @tag.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /tags/1 or /tags/1.json
+  # DELETE /tags/1
   def destroy
     @tag.destroy
 
     respond_to do |format|
       format.html { redirect_to tags_url, notice: "Tag was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
