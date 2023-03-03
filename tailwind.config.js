794c205a14beb0eda70ea9c123b3f2c0ee34b75b
Peter O'Hara Adu <ohara.invent@gmail.com>
2023-03-03 09:00:54 +0000

    Make uncategorized stand out

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/views/**/*.html.slim',
+    './app/views/**/*.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
